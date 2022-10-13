b0ab879a6a7b401ac32a5ee288117ea83083e453
rafael <matosrafael2005@gmail.com>
2022-10-12 23:23:50 -0300

    inclusão do padrão factory para criar proxies.

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,33 +2,36 @@ class NegociacaoController {
 
     constructor(){
         let $ = document.querySelector.bind(document);
-        
-        let self = this
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._listaNegociacao = 
-        
+
+        this._listaNegociacao = ProxyFactory.create(
+            new ListaNegociacoes(),
+            ['adiciona','esvazia'],
+            model => this._negociacaoView.update(model))
+    
         this._negociacaoView = new NegociacaoView($('#negociacaoView'))
         this._negociacaoView.update(this._listaNegociacao)
 
-        this._mensagem = new Mensagem();
+        this._mensagem = ProxyFactory.create(
+            new Mensagem(),
+            ['texto'],
+            model => this.MensagemView.update(model)
+        )
         this._mensagemView = new MensagemView($('#mensagemView'))
         this._mensagemView.update(this._mensagem)
     }
     adiciona(event) {
             event.preventDefault();
-
-            this._listaNegociacao.adiciona(this._criaNegociacao())
-            this._mensagem.texto = 'Negociação criada com sucesso!'
-            this._mensagemView.update(this._mensagem)
-            this._limpaFormulario()
+            this._listaNegociacao.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação criada com sucesso!';
+            this._limpaFormulario();
         }
     esvazia(){
-        this._listaNegociacao.esvazia()
-        this._mensagem.texto = 'Negociações removidas com sucesso.'
-        this._mensagemView.update(this._mensagem)
+        this._listaNegociacao.esvazia();
+        this._mensagem.texto = 'Negociações removidas com sucesso.';
+        
     }
     
     
--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -1,13 +1,13 @@
 class ProxyFactory{
     static create(objeto, props, acao){
-        
+
         return new Proxy(objeto, {
             get(target, prop, receiver){ 
-                if([props].includes(prop) && typeof(target[prop]) == typeof(Function)){
+                if(props.includes(prop) && typeof(target[prop]) == typeof(Function)){
                     return function(){
                         console.log(`${target[prop]} foi interceptado`);
                         Reflect.apply(target[prop], target, arguments)
-                        acao(target)
+                        return acao(target)
                     }
                 }
                 return Reflect.get(target, prop, receiver)
